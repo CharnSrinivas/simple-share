8487cd719b6ccc89c36f3106abd25d9e2f3b4122
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-05-11 14:43:27 +0530

    Successfully deployed backend

--- client/config.ts
+++ client/config.ts
@@ -1,2 +1,2 @@
-export const SignalingServerUrl = 'http://192.168.42.156:3000';
-//  "https://simple-share-app.herokuapp.com"
+export const SignalingServerUrl =  "https://simple-share-app.herokuapp.com"
+//  'http://192.168.42.156:3000';
